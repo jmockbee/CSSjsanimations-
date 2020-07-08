015fa356f0c0eeb25be385f3c8ea409450d0f327
Jon Mockbee <mockbee1000@gmail.com>
2020-07-08 12:37:21 -0400

    more animation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
-
-window.addEventListener('DOMContentLoaded', function (e) {
-  document.querySelector('hp-presentation').onclick = handlePresentationClick;
+window.addEventListener('DOMContentLoaded', function(e) {
+	document.querySelector('hp-presentation').onclick = handlePresentationClick;
 });
 
 function handlePresentationClick(e) {
-  var current = document.querySelector('hp-slide.active');
-  var next = current.nextElementSibling;
+	var current = document.querySelector('hp-slide.active');
+	var next = current.nextElementSibling;
 
-  if (next) {
-    current.classList.remove('active');
-    next.classList.add('active');
-  }
+	if (next) {
+		current.classList.remove('active');
+		next.classList.add('active');
+	}
 }
-
-
-
-
-
-
